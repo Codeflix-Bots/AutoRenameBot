4ac90dc0b92e6d0c092b8cbc1a6827ddd6e7cde1
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-12 14:11:18 +0530

    Update auto_rename.py

--- plugins/auto_rename.py
+++ plugins/auto_rename.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from helper.database import madflixbotz
+from helper.database import codeflixbots
 
 @Client.on_message(filters.private & filters.command("autorename"))
 async def auto_rename_command(client, message):
@@ -10,9 +10,9 @@ async def auto_rename_command(client, message):
     format_template = message.text.split("/autorename", 1)[1].strip()
 
     # Save the format template to the database
-    await madflixbotz.set_format_template(user_id, format_template)
+    await codeflixbots.set_format_template(user_id, format_template)
 
-    await message.reply_text("**Auto Rename Format Updated Successfully! ✅**")
+    await message.reply_text("**🌟 ꜰᴀɴᴛᴀꜱᴛɪᴄ! ʏᴏᴜ'ʀᴇ ʀᴇᴀᴅʏ ᴛᴏ ᴀᴜᴛᴏ-ʀᴇɴᴀᴍᴇ ʏᴏᴜʀ ꜰɪʟᴇꜱ ᴛᴏ:\n<code>{format_template}</code>.\n\n📩 ꜱɪᴍᴘʟʏ ꜱᴇɴᴅ ᴛʜᴇ ꜰɪʟᴇ(ꜱ) ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʀᴇɴᴀᴍᴇ.\n\nʀᴇᴍᴇᴍʙᴇʀ, ᴍᴀʏʙᴇ ɪ'ʟʟ ʀᴇɴᴀᴍᴇ ʏᴏᴜʀ ꜰɪʟᴇꜱ ꜱʟᴏᴡ ʙᴜᴛ ɪ ꜱᴜʀᴇʟʏ ᴍᴀᴋᴇ ᴛʜᴇᴍ ᴘᴇʀꜰᴇᴄᴛ!✨**")
 
 @Client.on_message(filters.private & filters.command("setmedia"))
 async def set_media_command(client, message):
@@ -20,16 +20,6 @@ async def set_media_command(client, message):
     media_type = message.text.split("/setmedia", 1)[1].strip().lower()
 
     # Save the preferred media type to the database
-    await madflixbotz.set_media_preference(user_id, media_type)
+    await codeflixbots.set_media_preference(user_id, media_type)
 
     await message.reply_text(f"**Media Preference Set To :** {media_type} ✅")
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper

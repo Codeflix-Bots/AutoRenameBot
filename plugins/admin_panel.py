893731bbce11c7768b029d94492316f44b73da75
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-12 14:05:19 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -1,5 +1,5 @@
 from config import Config, Txt
-from helper.database import madflixbotz
+from helper.database import codeflixbots
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
@@ -18,7 +18,7 @@ async def restart_bot(b, m):
     global is_restarting
     if not is_restarting:
         is_restarting = True
-        await m.reply_text("**🔄 Restarting.....**")
+        await m.reply_text("**ᴡᴀɪᴛ ʙʀᴏ...**")
 
         # Gracefully stop the bot's event loop
         b.stop()
@@ -36,32 +36,32 @@ async def tutorial(bot,message):
 	    text =Txt.FILE_NAME_TXT.format(format_template=format_template),
 	    disable_web_page_preview=True,
 	    reply_markup=InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/CallAdminRobot"), 
-        			InlineKeyboardButton("⚡ Tutorial",url = "https://t.me/MadflixBots_Support") ]])
+        			[InlineKeyboardButton("• ᴏᴡɴᴇʀ",url = "https://t.me/cosmic_freak"), 
+        			InlineKeyboardButton("• ᴛᴜᴛᴏʀɪᴀʟ",url = "https://t.me/codeflix_bots") ]])
 	)
 
 
 @Client.on_message(filters.command(["stats", "status"]) & filters.user(Config.ADMIN))
 async def get_stats(bot, message):
-    total_users = await madflixbotz.total_users_count()
+    total_users = await codeflixbots.total_users_count()
     uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))    
     start_t = time.time()
     st = await message.reply('**Accessing The Details.....**')    
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await st.edit(text=f"**--Bot Status--** \n\n**⌚️ Bot Uptime :** {uptime} \n**🐌 Current Ping :** `{time_taken_s:.3f} ms` \n**👭 Total Users :** `{total_users}`")
+    await st.edit(text=f"**--Bot Status--** \n\n**• Bot Uptime :** {uptime} \n**• Current Ping :** `{time_taken_s:.3f} ms` \n**• Total Users :** `{total_users}`")
 
 @Client.on_message(filters.command("broadcast") & filters.user(Config.ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
     await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Is Started The Broadcast......")
-    all_users = await madflixbotz.get_all_users()
+    all_users = await codeflixbots.get_all_users()
     broadcast_msg = m.reply_to_message
     sts_msg = await m.reply_text("Broadcast Started..!") 
     done = 0
     failed = 0
     success = 0
     start_time = time.time()
-    total_users = await madflixbotz.total_users_count()
+    total_users = await codeflixbots.total_users_count()
     async for user in all_users:
         sts = await send_msg(user['_id'], broadcast_msg)
         if sts == 200:
@@ -69,7 +69,7 @@ async def broadcast_handler(bot: Client, m: Message):
         else:
            failed += 1
         if sts == 400:
-           await madflixbotz.delete_user(user['_id'])
+           await codeflixbots.delete_user(user['_id'])
         done += 1
         if not done % 20:
            await sts_msg.edit(f"Broadcast In Progress: \n\nTotal Users {total_users} \nCompleted : {done} / {total_users}\nSuccess : {success}\nFailed : {failed}")
@@ -96,10 +96,3 @@ async def send_msg(user_id, message):
         logger.error(f"{user_id} : {e}")
         return 500
 
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper

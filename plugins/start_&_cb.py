4960fa8a511a3a43282ef6c014a57e33164844e8
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-12 15:28:49 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery, Message, InputMediaPhoto
 
 from helper.database import codeflixbots
-from config import Config, Txt  
+from config import *
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
@@ -51,6 +51,65 @@ async def cb_handler(client, query: CallbackQuery):
                 InlineKeyboardButton("ʙᴀᴄᴋ •", callback_data="help")
             ]])            
         )
+
+@Client.on_message(filters.command("donate"))
+async def donation(client, message):
+    btn = [[
+        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="help"),
+        InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url=f'https://t.me/sewxiy'),
+    ]]
+    yt=await message.reply_photo(photo='https://graph.org/file/1919fe077848bd0783d4c.jpg', caption=script.DONATE_TXT, reply_markup=InlineKeyboardMarkup(btn))
+    await asyncio.sleep(300)
+    await yt.delete()
+    await message.delete()
+
+@Client.on_message(filters.command("premium"))
+async def premium(bot, message):
+    btn = [[
+         InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/sewxuy"),
+         InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+    ]]
+    yt=await message.reply_photo(photo='https://graph.org/file/8b50e21db819f296661b7.jpg', caption=script.PREMIUM_TXT, reply_markup=InlineKeyboardMarkup(btn))
+    await asyncio.sleep(300)
+    await yt.delete()
+    await message.delete()
+
+@Client.on_message(filters.command("plan"))
+async def premium(bot, message):
+    btn = [[
+         InlineKeyboardButton("sᴇɴᴅ ᴘᴀʏᴍᴇɴᴛ sᴄʀᴇᴀɴsʜᴏᴛ ʜᴇʀᴇ", url="https://t.me/sewxuy"),
+         InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+    ]]
+    yt=await message.reply_photo(photo='https://graph.org/file/8b50e21db819f296661b7.jpg', caption=script.PREPLANS_TXT, reply_markup=InlineKeyboardMarkup(btn))
+    await asyncio.sleep(300)
+    await yt.delete()
+    await message.delete()
+
+@Client.on_message(filters.command("bought") & filters.private)
+async def bought(client, message):
+    msg = await message.reply('Wait im checking...')
+    replyed = message.reply_to_message
+    if not replyed:
+        await msg.edit("<b>Please reply with the screenshot of your payment for the premium purchase to proceed.\n\nFor example, first upload your screenshot, then reply to it using the '/bought' command</b>")
+    if replyed and replyed.photo:
+        await client.send_photo(
+            photo=replyed.photo.file_id,
+            chat_id=LOG_CHANNEL,
+            caption=f'<b>User - {message.from_user.mention}\nUser id - <code>{message.from_user.id}</code>\nusername - <code>{message.from_user.username}</code>\nUser Name - <code>{message.from_user.first_name}</code></b>',
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    
+                    [
+                        InlineKeyboardButton(
+                            "Close", callback_data="close_data"
+                        )
+                    ]
+                    
+                ]
+            )
+        )
+        await msg.edit_text('<b>Your screenshot has been sent to Admins</b>')
+
     elif data == "help":
         await query.message.edit_text(
             text=Txt.HELP_TXT.format(client.mention),

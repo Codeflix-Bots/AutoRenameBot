bdc8854c7add171a7ece97f4e62808277caa5d27
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-14 12:53:20 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -4,19 +4,20 @@ from helper.database import codeflixbots as db
 from config import Txt
 
 # Inline buttons for metadata toggle
-ON = [[InlineKeyboardButton('Metadata On ✅', callback_data='metadata_1')], 
-      [InlineKeyboardButton('Set Custom Metadata', callback_data='custom_metadata')]]
-OFF = [[InlineKeyboardButton('Metadata Off ❌', callback_data='metadata_0')], 
-       [InlineKeyboardButton('Set Custom Metadata', callback_data='custom_metadata')]]
+ON = [[InlineKeyboardButton('Metadata On ✅', callback_data='metadata_1')],
+      [InlineKeyboardButton('Metadata Commands', callback_data='metadata_commands')]]
+OFF = [[InlineKeyboardButton('Metadata Off ❌', callback_data='metadata_0')],
+       [InlineKeyboardButton('Metadata Commands', callback_data='metadata_commands')]]
 
-# Buttons for custom metadata settings
-METADATA_BUTTONS = [
+# Buttons for metadata commands
+METADATA_COMMANDS_BUTTONS = [
     [InlineKeyboardButton('Set Title', callback_data='set_title')],
     [InlineKeyboardButton('Set Author', callback_data='set_author')],
     [InlineKeyboardButton('Set Artist', callback_data='set_artist')],
     [InlineKeyboardButton('Set Audio', callback_data='set_audio')],
     [InlineKeyboardButton('Set Subtitle', callback_data='set_subtitle')],
-    [InlineKeyboardButton('Set Video', callback_data='set_video')]
+    [InlineKeyboardButton('Set Video', callback_data='set_video')],
+    [InlineKeyboardButton('Back to Metadata', callback_data='back_to_metadata')]
 ]
 
 @Client.on_message(filters.private & filters.command('metadata'))
@@ -26,19 +27,12 @@ async def handle_metadata(bot: Client, message: Message):
     user_metadata = await db.get_metadata_code(message.from_user.id)
     await ms.delete()
     
-    metadata_display = f"Title: `{await db.get_title(message.from_user.id)}`\n" \
-                       f"Author: `{await db.get_author(message.from_user.id)}`\n" \
-                       f"Artist: `{await db.get_artist(message.from_user.id)}`\n" \
-                       f"Audio: `{await db.get_audio(message.from_user.id)}`\n" \
-                       f"Subtitle: `{await db.get_subtitle(message.from_user.id)}`\n" \
-                       f"Video: `{await db.get_video(message.from_user.id)}`"
-
     if bool_metadata:
-        return await message.reply_text(f"Your Current Metadata:\n\n{metadata_display}", reply_markup=InlineKeyboardMarkup(ON))
-    
-    return await message.reply_text(f"Your Current Metadata:\n\n{metadata_display}", reply_markup=InlineKeyboardMarkup(OFF))
+        await message.reply_text(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(ON))
+    else:
+        await message.reply_text(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(OFF))
 
-@Client.on_callback_query(filters.regex('^metadata_'))
+@Client.on_callback_query(filters.regex('.*?(metadata|set_).*?'))
 async def query_metadata(bot: Client, query: CallbackQuery):
     data = query.data
     user_metadata = await db.get_metadata_code(query.from_user.id)
@@ -48,41 +42,45 @@ async def query_metadata(bot: Client, query: CallbackQuery):
         
         if bool_meta:
             await db.set_metadata(query.from_user.id, False)
-            await query.message.edit(f"Your Current Metadata:\n\n{metadata_display}", reply_markup=InlineKeyboardMarkup(OFF))
+            await query.message.edit(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(OFF))
         else:
             await db.set_metadata(query.from_user.id, True)
-            await query.message.edit(f"Your Current Metadata:\n\n{metadata_display}", reply_markup=InlineKeyboardMarkup(ON))
+            await query.message.edit(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(ON))
 
-    elif data == 'custom_metadata':
-        await query.message.edit("**Select Metadata to Set:**", reply_markup=InlineKeyboardMarkup(METADATA_BUTTONS))
+    elif data == 'metadata_commands':
+        await query.message.edit("**Select Metadata Command to Set:**", reply_markup=InlineKeyboardMarkup(METADATA_COMMANDS_BUTTONS))
 
-@Client.on_callback_query(filters.regex('^set_'))
-async def set_metadata_type(bot: Client, query: CallbackQuery):
-    metadata_type = query.data.split('_')[1]
-    await query.message.delete()
-    
-    metadata_prompt = {
-        'title': 'Gɪᴠᴇ Tʜᴇ Tɪᴛʟᴇ',
-        'author': 'Gɪᴠᴇ Tʜᴇ Aᴜᴛʜᴏʀ',
-        'artist': 'Gɪᴠᴇ Tʜᴇ Aʀᴛɪsᴛ',
-        'audio': 'Gɪᴠᴇ Tʜᴇ Aᴜᴅɪᴏ Tɪᴛʟᴇ',
-        'subtitle': 'Gɪᴠᴇ Tʜᴇ Sᴜʙᴛɪᴛʟᴇ',
-        'video': 'Gɪᴠᴇ Tʜᴇ Vɪᴅᴇᴏ Tɪᴛʟᴇ'
-    }
-    
-    await query.message.reply_text(metadata_prompt[metadata_type], reply_markup=InlineKeyboardMarkup([]))  # Send prompt
-    await db.set_metadata_code(query.from_user.id, metadata_type)  # Set the pending metadata type
+    elif data == 'back_to_metadata':
+        bool_metadata = await db.get_metadata(query.from_user.id)
+        user_metadata = await db.get_metadata_code(query.from_user.id)
+        if bool_metadata:
+            await query.message.edit(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(ON))
+        else:
+            await query.message.edit(f"Your Current Metadata:\n\n➜ `{user_metadata}`", reply_markup=InlineKeyboardMarkup(OFF))
+
+    elif data.startswith('set_'):
+        metadata_type = data.split('_')[1]
+        metadata_prompt = {
+            'title': 'Gɪᴠᴇ Tʜᴇ Tɪᴛʟᴇ',
+            'author': 'Gɪᴠᴇ Tʜᴇ Aᴜᴛʜᴏʀ',
+            'artist': 'Gɪᴠᴇ Tʜᴇ Aʀᴛɪsᴛ',
+            'audio': 'Gɪᴠᴇ Tʜᴇ Aᴜᴅɪᴏ Tɪᴛʟᴇ',
+            'subtitle': 'Gɪᴠᴇ Tʜᴇ Sᴜʙᴛɪᴛʟᴇ',
+            'video': 'Gɪᴠᴇ Tʜᴇ Vɪᴅᴇᴏ Tɪᴛʟᴇ'
+        }
+        await query.message.reply_text(metadata_prompt[metadata_type], reply_markup=InlineKeyboardMarkup([]))  # Send prompt
+        await db.set_metadata_code(query.from_user.id, metadata_type)  # Set the pending metadata type
 
 @Client.on_message(filters.private & filters.text)
 async def handle_user_response(bot: Client, message: Message):
     user_id = message.from_user.id
-    pending_metadata_type = await db.get_metadata_code(user_id)
+    pending_metadata_code = await db.get_metadata_code(user_id)
     
-    if pending_metadata_type:
-        # Ensure pending_metadata_type is clean
-        if pending_metadata_type in ['title', 'author', 'artist', 'audio', 'subtitle', 'video']:
-            await getattr(db, f'set_{pending_metadata_type}')(user_id, message.text)
-            await message.reply_text(f"**Your Metadata Code `{pending_metadata_type}` Set Successfully ✅**")
+    if pending_metadata_code:
+        # Ensure pending_metadata_code is clean
+        if pending_metadata_code in ['title', 'author', 'artist', 'audio', 'subtitle', 'video']:
+            await getattr(db, f'set_{pending_metadata_code}')(user_id, message.text)
+            await message.reply_text(f"**Your Metadata `{pending_metadata_code}` Set Successfully ✅**")
         else:
             await message.reply_text("⚠️ Invalid metadata type. Please try again.")
         await db.set_metadata_code(user_id, None)  # Clear pending metadata type
@@ -124,10 +122,10 @@ async def set_metadata(bot: Client, message: Message, metadata_type: str):
 
     command_parts = message.text.split(f"/set{metadata_type}", 1)
     if len(command_parts) < 2 or not command_parts[1].strip():
-        await message.reply_text(f"**Give the {metadata_type.capitalize()}**\n\n"
+        await message.reply_text(f"**Please provide a {metadata_type} after the command /set{metadata_type}.**\n\n"
                                  f"**Example:** `/set{metadata_type} My Custom {metadata_type.capitalize()}`")
         return
 
     metadata_value = command_parts[1].strip()
     await getattr(db, f'set_{metadata_type}')(user_id, metadata_value)
-    await message.reply_text(f"**Your Metadata Code `{metadata_type}` Set Successfully ✅**")
+    await message.reply_text(f"**{metadata_type.capitalize()} has been set to:** `{metadata_value}`")

66d3bfda2da6299642e05c341be934f45bc3de05
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-23 18:37:44 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -13,31 +13,31 @@ from config import Txt, Config
 # AUTH_USERS = Config.AUTH_USERS
 
 ON = [
-    [InlineKeyboardButton("Metadata On ✅", callback_data="metadata_1")],
-    [InlineKeyboardButton("Set Custom Metadata", callback_data="cutom_metadata")],
+    [InlineKeyboardButton("ᴍᴇᴛᴀᴅᴀᴛᴀ ᴏɴ ✅", callback_data="metadata_1")],
+    [InlineKeyboardButton("sᴇᴛ ᴄᴜsᴛᴏᴍ ᴍᴇᴛᴀᴅᴀᴛᴀ", callback_data="cutom_metadata")],
 ]
 OFF = [
-    [InlineKeyboardButton("Metadata Off ❌", callback_data="metadata_0")],
-    [InlineKeyboardButton("Set Custom Metadata", callback_data="cutom_metadata")],
+    [InlineKeyboardButton("ᴍᴇᴛᴀᴅᴀᴛᴀ ᴏғғ ❌", callback_data="metadata_0")],
+    [InlineKeyboardButton("sᴇᴛ ᴄᴜsᴛᴏᴍ ᴍᴇᴛᴀᴅᴀᴛᴀ", callback_data="cutom_metadata")],
 ]
 
 
 @Client.on_message(filters.private & filters.command("metadata"))
 async def handle_metadata(bot: Client, message: Message):
 
-    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    ms = await message.reply_text("**Wait A Second..**", reply_to_message_id=message.id)
     bool_metadata = await codeflixbots.get_metadata(message.from_user.id)
     user_metadata = await codeflixbots.get_metadata_code(message.from_user.id)
     await ms.delete()
     if bool_metadata:
 
         return await message.reply_text(
-            f"<b>Your Current Metadata:</b>\n\n➜ `{user_metadata}` ",
+            f"<b>ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴍᴇᴛᴀᴅᴀᴛᴀ:</b>\n\n➜ `{user_metadata}` ",
             reply_markup=InlineKeyboardMarkup(ON),
         )
 
     return await message.reply_text(
-        f"<b>Your Current Metadata:</b>\n\n➜ `{user_metadata}` ",
+        f"<b>ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴍᴇᴛᴀᴅᴀᴛᴀ:</b>\n\n➜ `{user_metadata}` ",
         reply_markup=InlineKeyboardMarkup(OFF),
     )
 
@@ -54,14 +54,14 @@ async def query_metadata(bot: Client, query: CallbackQuery):
         if bool(eval(_bool)):
             await codeflixbots.set_metadata(query.from_user.id, bool_meta=False)
             await query.message.edit(
-                f"<b>Your Current Metadata:</b>\n\n➜ `{user_metadata}` ",
+                f"<b>ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴍᴇᴛᴀᴅᴀᴛᴀ:</b>\n\n➜ `{user_metadata}` ",
                 reply_markup=InlineKeyboardMarkup(OFF),
             )
 
         else:
             await codeflixbots.set_metadata(query.from_user.id, bool_meta=True)
             await query.message.edit(
-                f"<b>Your Current Metadata:</b>\n\n➜ `{user_metadata}` ",
+                f"<b>ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴍᴇᴛᴀᴅᴀᴛᴀ:</b>\n\n➜ `{user_metadata}` ",
                 reply_markup=InlineKeyboardMarkup(ON),
             )
 
@@ -84,7 +84,7 @@ async def query_metadata(bot: Client, query: CallbackQuery):
                 return
             print(metadata.text)
             ms = await query.message.reply_text(
-                "**Please Wait...**", reply_to_message_id=metadata.id
+                "**Wait A Second..**", reply_to_message_id=metadata.id
             )
             await codeflixbots.set_metadata_code(
                 query.from_user.id, metadata_code=metadata.text

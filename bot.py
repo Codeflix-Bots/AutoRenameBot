519d3639287192e2a791c9e119f762fc5e182303
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-26 15:40:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,15 +36,42 @@ class Bot(Client):
             await app.setup()       
             await web.TCPSite(app, "0.0.0.0", 8080).start()     
         print(f"{me.first_name} Is Started.....✨️")
-        for id in Config.ADMIN:
-            try: await self.send_message(Config.LOG_CHANNEL, f"**{me.first_name}  Is Started.....✨️**")                                
-            except: pass
+
+        # Define the inline buttons
+        buttons = [
+            [
+                InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/sewxiy"),
+                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/codeflix_bots")
+            ]
+        ]
+
+        # Send the message with the image and buttons
+        image_url = "https://graph.org/file/a27d85469761da836337c.jpg"
+        message_text = "ᴀɴʏᴀ ɪs ʀᴇsᴛᴀʀᴛᴇᴅ ᴀɢᴀɪɴ  !"
+        
+        for chat_id in [Config.LOG_CHANNEL, Config.SUPPORT_CHAT]:
+            try:
+                await self.send_photo(
+                    chat_id,
+                    photo=image_url,
+                    caption=message_text,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            except Exception as e:
+                print(f"Failed to send message to {chat_id}: {e}")
+
         if Config.LOG_CHANNEL:
             try:
                 curr = datetime.now(timezone("Asia/Kolkata"))
                 date = curr.strftime('%d %B, %Y')
                 time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+                await self.send_message(Config.LOG_CHANNEL, 
+                    f"**{me.mention} Is Restarted !!**\n\n"
+                    f"📅 Date : `{date}`\n"
+                    f"⏰ Time : `{time}`\n"
+                    f"🌐 Timezone : `Asia/Kolkata`\n\n"
+                    f"🉐 Version : `v{__version__} (Layer {layer})`</b>"
+                )
             except:
                 print("Please Make This Is Admin In Your Log Channel")
 

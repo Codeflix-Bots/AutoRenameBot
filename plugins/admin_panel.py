9a921fc499e95225ecbf636e333e628a5701f89e
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-09-16 16:14:26 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -3,7 +3,7 @@ from helper.database import codeflixbots
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
-import os, sys, time, asyncio, logging, datetime, traceback
+import os, sys, time, asyncio, logging, datetime
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 logger = logging.getLogger(__name__)
@@ -14,11 +14,11 @@ ADMIN_USER_ID = Config.ADMIN
 is_restarting = False
 
 @Client.on_message(filters.private & filters.command("restart") & filters.user(ADMIN_USER_ID))
-async def restart_bot(b: Client, m: Message):
+async def restart_bot(b, m):
     global is_restarting
     if not is_restarting:
         is_restarting = True
-        await m.reply_text("**ᴘʀᴏᴄᴇssᴇs sᴛᴏᴘᴘᴇᴅ. ʙᴏᴛ ɪs ʀᴇsᴛᴀʀᴛɪɴɢ.....**")
+        await m.reply_text("**Restarting.....**")
 
         # Gracefully stop the bot's event loop
         b.stop()
@@ -27,136 +27,33 @@ async def restart_bot(b: Client, m: Message):
         # Restart the bot process
         os.execl(sys.executable, sys.executable, *sys.argv)
 
-@Client.on_message(filters.private & filters.command("tutorial"))
-async def tutorial(bot: Client, message: Message):
-    user_id = message.from_user.id
-    format_template = await codeflixbots.get_format_template(user_id)
-    await message.reply_text(
-        text=Txt.FILE_NAME_TXT.format(format_template=format_template),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("• ᴏᴡɴᴇʀ", url="https://t.me/cosmic_freak"),
-             InlineKeyboardButton("• ᴛᴜᴛᴏʀɪᴀʟ", url="https://t.me/codeflix_bots")]
-        ])
-    )
 
-@Client.on_message(filters.private & filters.command("ban") & filters.user(Config.ADMIN))
-async def ban(c: Client, m: Message):
-    if len(m.command) < 4:
-        await m.reply_text(
-            f"Use this command to ban any user from the bot.\n\n"
-            f"Usage:\n\n"
-            f"`/ban user_id ban_duration ban_reason`\n\n"
-            f"Eg: `/ban 1234567 28 You misused me.`\n"
-            f"This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
-            quote=True
-        )
-        return
+@Client.on_message(filters.private & filters.command(["tutorial"]))
+async def tutorial(bot,message):
+	user_id = message.from_user.id
+	format_template = await codeflixbots.get_format_template(user_id)
+	await message.reply_text(
+	    text =Txt.FILE_NAME_TXT.format(format_template=format_template),
+	    disable_web_page_preview=True,
+	    reply_markup=InlineKeyboardMarkup([
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/"), 
+        			InlineKeyboardButton("⚡ Tutorial",url = "https://t.me/") ]])
+	)
 
-    try:
-        user_id = int(m.command[1])
-        ban_duration = int(m.command[2])
-        ban_reason = ' '.join(m.command[3:])
-        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
-        
-        try:
-            await c.send_message(
-                user_id,
-                f"You are banned from using this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__.\n\n**Message from the admin**"
-            )
-            ban_log_text += '\n\nUser notified successfully!'
-        except Exception:
-            traceback.print_exc()
-            ban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
-
-        await codeflixbots.ban_user(user_id, ban_duration, ban_reason)
-        logger.info(ban_log_text)
-        await m.reply_text(ban_log_text, quote=True)
-    except Exception:
-        traceback.print_exc()
-        await m.reply_text(
-            f"Error occurred! Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-@Client.on_message(filters.private & filters.command("unban") & filters.user(Config.ADMIN))
-async def unban(c: Client, m: Message):
-    if len(m.command) != 2:
-        await m.reply_text(
-            f"Use this command to unban any user.\n\n"
-            f"Usage:\n\n`/unban user_id`\n\n"
-            f"Eg: `/unban 1234567`\n"
-            f"This will unban user with id `1234567`.",
-            quote=True
-        )
-        return
-
-    try:
-        user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user {user_id}"
-        
-        try:
-            await c.send_message(
-                user_id,
-                "Your ban was lifted!"
-            )
-            unban_log_text += '\n\nUser notified successfully!'
-        except Exception:
-            traceback.print_exc()
-            unban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
-
-        await codeflixbots.remove_ban(user_id)
-        logger.info(unban_log_text)
-        await m.reply_text(unban_log_text, quote=True)
-    except Exception:
-        traceback.print_exc()
-        await m.reply_text(
-            f"Error occurred! Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-@Client.on_message(filters.private & filters.command("banned_users") & filters.user(Config.ADMIN))
-async def banned_users(_, m: Message):
-    all_banned_users = await codeflixbots.get_all_banned_users()
-    banned_usr_count = 0
-    text = ''
-
-    async for banned_user in all_banned_users:
-        user_id = banned_user['id']
-        ban_duration = banned_user['ban_status']['ban_duration']
-        banned_on = banned_user['ban_status']['banned_on']
-        ban_reason = banned_user['ban_status']['ban_reason']
-        banned_usr_count += 1
-        text += f"> **user_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, " \
-                f"**Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-
-    reply_text = f"Total banned user(s): `{banned_usr_count}`\n\n{text}"
-    if len(reply_text) > 4096:
-        with open('banned-users.txt', 'w') as f:
-            f.write(reply_text)
-        await m.reply_document('banned-users.txt', caption="Banned users list")
-        os.remove('banned-users.txt')
-    else:
-        await m.reply_text(reply_text, quote=True)
 
 @Client.on_message(filters.command(["stats", "status"]) & filters.user(Config.ADMIN))
-async def get_stats(bot: Client, message: Message):
+async def get_stats(bot, message):
     total_users = await codeflixbots.total_users_count()
-    uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot.uptime))
+    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))    
     start_t = time.time()
-    st = await message.reply('**Accessing The Details.....**')
+    st = await message.reply('**Accessing The Details.....**')    
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await st.edit_text(
-        f"**--Bot Status--** \n\n"
-        f"**⌚️ Bot Uptime :** {uptime} \n"
-        f"**🐌 Current Ping :** `{time_taken_s:.3f} ms` \n"
-        f"**👭 Total Users :** `{total_users}`"
-    )
+    await st.edit(text=f"**--Bot Status--** \n\n**⌚️ Bot Uptime :** {uptime} \n**🐌 Current Ping :** `{time_taken_s:.3f} ms` \n**👭 Total Users :** `{total_users}`")
 
 @Client.on_message(filters.command("broadcast") & filters.user(Config.ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
-    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Started the Broadcast.")
+    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Is Started The Broadcast......")
     all_users = await codeflixbots.get_all_users()
     broadcast_msg = m.reply_to_message
     sts_msg = await m.reply_text("Broadcast Started..!") 
@@ -165,29 +62,27 @@ async def broadcast_handler(bot: Client, m: Message):
     success = 0
     start_time = time.time()
     total_users = await codeflixbots.total_users_count()
-    
     async for user in all_users:
         sts = await send_msg(user['_id'], broadcast_msg)
         if sts == 200:
-            success += 1
-        elif sts == 400:
-            await codeflixbots.delete_user(user['_id'])
+           success += 1
         else:
-            failed += 1
+           failed += 1
+        if sts == 400:
+           await codeflixbots.delete_user(user['_id'])
         done += 1
         if not done % 20:
-            await sts_msg.edit_text(f"Broadcast In Progress: \n\nTotal Users: {total_users} \nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
-
+           await sts_msg.edit(f"Broadcast In Progress: \n\nTotal Users {total_users} \nCompleted : {done} / {total_users}\nSuccess : {success}\nFailed : {failed}")
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await sts_msg.edit_text(f"Bʀᴏᴀᴅᴄᴀsᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCompleted in `{completed_in}`.\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
-
-async def send_msg(user_id: int, message: Message):
+    await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")
+           
+async def send_msg(user_id, message):
     try:
-        await message.copy(chat_id=user_id)
+        await message.copy(chat_id=int(user_id))
         return 200
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        return await send_msg(user_id, message)
+        return send_msg(user_id, message)
     except InputUserDeactivated:
         logger.info(f"{user_id} : Deactivated")
         return 400
